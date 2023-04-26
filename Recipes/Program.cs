24ee68fed9150a8a29dcdec0845db845b82bf0bf
tshebo <120386361+tshebo@users.noreply.github.com>
2023-04-26 09:37:56 +0200

    validateChoice Method. This method checks the user input and calls the relevant functions

--- Recipes/Program.cs
+++ Recipes/Program.cs
@@ -9,8 +9,11 @@
             Menu();
         } 
     }
+
     static string Menu()
     {
+        string choice = "";
+
         Console.ForegroundColor = ConsoleColor.Blue;
         Console.WriteLine("=============================Welcome to the recipe app choose between the provided options==============================");
         Console.ForegroundColor = ConsoleColor.White;
@@ -18,11 +21,42 @@
         Console.WriteLine("1. Add ingredients");
         Console.WriteLine("2. Add steps");
         Console.WriteLine("3. Display recipe ");
-        Console.WriteLine("4. Clear Data ");
+        Console.WriteLine("4. Scaling Options ");
+        Console.WriteLine("5. Clear Data ");
         Console.WriteLine("Any key to Exit");
 
         Console.ForegroundColor = ConsoleColor.Blue;
-        return Console.ReadLine();
+        choice =Console.ReadLine();
+        Console.ForegroundColor = ConsoleColor.Blue;
+        return choice;
+    }
+
+    static void validateChoice(string menu)
+    {
+        if (menu.Equals("1"))
+        {
+            Console.WriteLine("===== Add Ingredients =====");
+        }
+        else if(menu.Equals("2"))
+        {
+            Console.WriteLine("===== Add Steps =====");
+        }
+        else if (menu.Equals("3"))
+        {
+            Console.WriteLine("===== Display Recipe =====");
+        }
+        else if (menu.Equals("4"))
+        {
+            Console.WriteLine("===== Scaling Options =====");
+        }
+        else if (menu.Equals("5"))
+        {
+            Console.WriteLine("===== Clear Data =====");
+        }
+        else  
+        {
+            Console.WriteLine("===== Exit App =====");
+        }
     }
 
     
